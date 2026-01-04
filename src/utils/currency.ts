fef56c4bb04bd6651a766071bab3b5f9cb1e17a9
Darren Lester <darren@darrenlester.com>
2026-01-04 13:48:46 +0000

    Use standard short currency formatting

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -1,11 +1,2 @@
 export const currencyFormatter = new Intl.NumberFormat("en-GB", { style: "currency", currency: "GBP", maximumFractionDigits: 0 });
-
-export function abbreviateCurrency(value: number) {
-    if (value >= 1000000) {
-        return currencyFormatter.format(value / 1000000) + "M";
-    } else if (value >= 1000) {
-        return currencyFormatter.format(value / 1000) + "K";
-    } else {
-        return currencyFormatter.format(value);
-    }
-}
+export const shortCurrencyFormatter = new Intl.NumberFormat("en-GB", { style: "currency", currency: "GBP", notation: "compact", compactDisplay: "short" });
